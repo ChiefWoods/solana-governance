6cc22ac49c62bcb03e7a2d2a0265de0218d2913f
Chii Yuen <chii.yuen@hotmail.com>
2026-08-13 22:00:49 +0800

    chore(frontend): add defaults to rpc urls

--- frontend/src/env.ts
+++ frontend/src/env.ts
@@ -18,9 +18,18 @@ export const env = createEnv({
    */
   client: {
     NEXT_PUBLIC_SENTRY_DSN: z.string().optional(),
-    NEXT_PUBLIC_SOLANA_RPC_MAINNET: z.string().url().optional(),
-    NEXT_PUBLIC_SOLANA_RPC_TESTNET: z.string().url().optional(),
-    NEXT_PUBLIC_SOLANA_RPC_DEVNET: z.string().url().optional(),
+    NEXT_PUBLIC_SOLANA_RPC_MAINNET: z
+      .string()
+      .url()
+      .default("https://api.mainnet-beta.solana.com"),
+    NEXT_PUBLIC_SOLANA_RPC_TESTNET: z
+      .string()
+      .url()
+      .default("https://api.testnet.solana.com"),
+    NEXT_PUBLIC_SOLANA_RPC_DEVNET: z
+      .string()
+      .url()
+      .default("https://api.devnet.solana.com"),
   },
 
   /**
