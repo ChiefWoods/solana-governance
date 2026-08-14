5c4319c549c13cf5f351ffd00ceedf0e473809d8
Chii Yuen <chii.yuen@hotmail.com>
2026-08-15 01:44:24 +0800

    chore(frontend): add oxfmt

---
+++ frontend/oxfmt.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from "oxfmt";
+
+import rootConfig from "../oxfmt.config.ts";
+
+export default defineConfig({
+  ...rootConfig,
+  ignorePatterns: [...(rootConfig.ignorePatterns ?? []), ".next/**", "out/**", "build/**", "next-env.d.ts"],
+});
