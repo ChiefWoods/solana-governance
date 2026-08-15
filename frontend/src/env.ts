8b07faf2081e5acc0c078c95d31f0d21160215f2
Chii Yuen <chii.yuen@hotmail.com>
2026-08-15 16:23:09 +0800

    feat(frontend): add docs env field

--- frontend/src/env.ts
+++ frontend/src/env.ts
@@ -7,12 +7,14 @@ export const env = createEnv({
     },
     client: {
         NEXT_PUBLIC_SENTRY_DSN: z.string().optional(),
+        NEXT_PUBLIC_DOCS_URL: z.string().url().default('https://docs.governance.solana.com'),
         NEXT_PUBLIC_SOLANA_RPC_MAINNET: z.string().url().default('https://api.mainnet-beta.solana.com'),
         NEXT_PUBLIC_SOLANA_RPC_TESTNET: z.string().url().default('https://api.testnet.solana.com'),
         NEXT_PUBLIC_SOLANA_RPC_DEVNET: z.string().url().default('https://api.devnet.solana.com'),
     },
     runtimeEnv: {
         NEXT_PUBLIC_SENTRY_DSN: process.env.NEXT_PUBLIC_SENTRY_DSN,
+        NEXT_PUBLIC_DOCS_URL: process.env.NEXT_PUBLIC_DOCS_URL,
         SENTRY_AUTH_TOKEN: process.env.SENTRY_AUTH_TOKEN,
         NEXT_PUBLIC_SOLANA_RPC_MAINNET: process.env.NEXT_PUBLIC_SOLANA_RPC_MAINNET,
         NEXT_PUBLIC_SOLANA_RPC_TESTNET: process.env.NEXT_PUBLIC_SOLANA_RPC_TESTNET,
